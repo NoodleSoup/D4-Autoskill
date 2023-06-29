e898601a546042f4e575fe184c1266038746b5ac
Daan Eekhof <daan@snakeware.nl>
2023-06-29 17:48:00 +0200

    dynamically get PID using WMI
    - set childprocess=False (this improves performance on my end)
    - use discard variables for unused variable names
    - remove some logging when a skill has been found or not (irrelevant imo)
    - remove check if a skill has been enabled from the config, all options are deemed enabled from the config.json

--- autoskill.py
+++ autoskill.py
@@ -6,56 +6,61 @@ import json
 import time
 import ctypes
 from colorama import init, Fore
+import wmi
 
-TARGET_PID = 4388
 init(autoreset=True)  # Initialize colorama
+f = wmi.WMI()
 
 def load_config():
     with open("config.json", "r") as f:
         return json.load(f)
 
-def find_image_on_screen(template, threshold=0.9):
-    screenshot = ImageGrab.grab()
+def find_image_on_screen(template, threshold=0.7):
+    screenshot = ImageGrab.grab(childprocess=False)
     screen = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
     result = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
     if max_val >= threshold:
         return max_loc
     return None
 
-def is_target_window_active():
+def is_target_window_active(target_pid):
     GetForegroundWindow = ctypes.windll.user32.GetForegroundWindow
     GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
 
     hwnd = GetForegroundWindow()
     pid = ctypes.wintypes.DWORD()
     GetWindowThreadProcessId(hwnd, ctypes.pointer(pid))
-    return pid.value == TARGET_PID
+    return pid.value == target_pid
+
+def find_process(name = "Diablo IV.exe"):
+    process = [proc for proc in f.Win32_Process() if proc.Name == name]
+    if (len(process) == 0):
+        return
+    return process[0].ProcessId
 
 def main():
     config = load_config()
+    found_proc = False
+    process_id = 0
+
+    while not found_proc:
+        process_id = find_process()
+        if process_id:
+            found_proc = True
 
     while True:
-        if not is_target_window_active():
+        if not is_target_window_active(process_id):
             print(Fore.YELLOW + "Target window is not active")
             time.sleep(1)
             continue
 
-        match_found = False
         for match in config["matches"]:
-            if not match["enabled"]:
-                continue
-
             image = cv2.imread(match["image"], cv2.IMREAD_COLOR)
             if find_image_on_screen(image) is not None:
-                match_found = True
-                print(Fore.GREEN + f"Match found for {match['image']}, pressing key {match['key']}")
-                pyautogui.press(match["key"])
-
-        if not match_found:
-            print(Fore.RED + "No match found")
+                pyautogui.press(match["key"], 3, 0.0)
 
         time.sleep(0.1)
 
